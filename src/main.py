3d7a36dccd1ff8843a6b4876bc47cc854798b686
autosat-sdc <noreply@example.com>
2025-02-25 16:57:41 +0100

    feat: modified main to test controller

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
+from time import process_time_ns
+
 from src.gamepad import Gamepad
 
 
 def main():
-    gamepad = Gamepad.XboxONE()
+    gamepad = Gamepad.Xbox360()
     gamepad.startBackgroundUpdates()
+    while gamepad.isConnected():
+        # Wait for the next event
+        eventType, control, value = gamepad.getNextEvent()
+
+        # Determine the type
+        if eventType == 'BUTTON':
+            print(f'{control} {value}')
+
+        elif eventType == 'AXIS':
+            print(f'{control} {value}')
     gamepad.disconnect()
 
 if __name__ == "__main__":
