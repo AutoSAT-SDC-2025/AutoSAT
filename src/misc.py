728910795b2a05dbe9765ca63f9104656b64ab03
mihneabastea <37101806+0Unkn0wn@users.noreply.github.com>
2025-03-13 01:29:40 +0100

    feat: added functions to calculate throttle and steering from controller inputs

--- src/misc.py
+++ src/misc.py
@@ -1,7 +1,20 @@
 import numpy
+from src.car_variables import CarType
 
-def calcAxisAngle(axis_position):
-    if axis_position[0] is 0.0:
+async def calc_axis_angle(x_axis: float, y_axis: float):
+    if x_axis == 0.0:
         return 0.0
     else:
-        return numpy.atan(axis_position[1]/axis_position[0])
+        return round(numpy.atan2(y_axis,x_axis),2)
+
+def calculate_hunter_throttle(controller_axis_value: float, car_type: CarType) -> float | None:
+    if car_type == CarType.kart:
+        return controller_axis_value * 100
+    elif car_type == CarType.hunter:
+        return controller_axis_value * 1500
+
+def calculate_hunter_steering(controller_axis_value: float, car_type: CarType) -> float | None:
+    if car_type == CarType.kart:
+        return controller_axis_value * 1.25
+    elif car_type == CarType.hunter:
+        return controller_axis_value * 576
