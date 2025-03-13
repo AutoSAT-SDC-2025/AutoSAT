b849730c759ed2b675ec20ff514eb4bd95c12c74
autosat-sdc-mihnea <noreply@example.com>
2025-03-13 01:28:38 +0100

    feat: added aliases for controller inputs

---
+++ src/gamepad/controller_mapping.py
@@ -0,0 +1,9 @@
+from enum import StrEnum
+
+class ControllerMapping(StrEnum):
+    buttonExit = 'B'
+    L_joystickX = 'LEFT-X'
+    L_joystickY = 'LEFT-Y'
+    R_joystickX = 'RIGHT-X'
+    R_joystickY = 'RIGHT-Y'
+    throttle = 'RT'
