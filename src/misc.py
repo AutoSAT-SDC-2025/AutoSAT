6d4b07fc7592d227f8467a75f7e1977315b0d2f9
mihneabastea <37101806+0Unkn0wn@users.noreply.github.com>
2025-04-02 20:00:08 +0200

    feat: added cubic scaling to steering and throttle and removed unused function

--- src/misc.py
+++ src/misc.py
@@ -1,20 +1,16 @@
 import numpy
 from src.car_variables import CarType
 
-async def calc_axis_angle(x_axis: float, y_axis: float):
-    if x_axis == 0.0:
-        return 0.0
-    else:
-        return round(numpy.atan2(y_axis,x_axis),2)
-
-def calculate_hunter_throttle(controller_axis_value: float, car_type: CarType) -> float | None:
+def calculate_throttle(controller_axis_value: float, car_type: CarType) -> float | None:
     if car_type == CarType.kart:
-        return -controller_axis_value * 100
+        x = max(0.0, controller_axis_value)
+        return (x ** 3) * 100
     elif car_type == CarType.hunter:
-        return -controller_axis_value * 1500
+        return -(controller_axis_value ** 3) * 1500
 
-def calculate_hunter_steering(controller_axis_value: float, car_type: CarType) -> float | None:
+def calculate_steering(controller_axis_value: float, car_type: CarType) -> float | None:
+    cubic = -(controller_axis_value ** 3)
     if car_type == CarType.kart:
-        return -controller_axis_value * 1.25
+        return cubic * 1.25
     elif car_type == CarType.hunter:
-        return -controller_axis_value * 576
+        return cubic * 576
