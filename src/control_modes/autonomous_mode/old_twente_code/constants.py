e5e97a91c5df28dc830b2bd5f9bd44eba0d3c129
autosat-sdc-mihnea <noreply@example.com>
2025-04-03 04:26:57 +0200

    feat: progress into breaking up the old code

---
+++ src/control_modes/autonomous_mode/old_twente_code/constants.py
@@ -0,0 +1,8 @@
+CAN_MSG_SENDING_SPEED = 0.040
+height = 480
+width = 848
+scale = 1
+OBJECT_FPS_CAP = 20
+OBJECT_DOWNSAMPLE_FACTOR = 1
+LOG_ROOT = "logs"
+FALLBACK_VIDEO_PATH = "src/control_modes/autonomous_mode/old_twente_code/recording 13-06-2024 11-54-31.mp4"
