001dd92915882745ce6ad5020f36951334b347cf
mihneabastea <37101806+0Unkn0wn@users.noreply.github.com>
2025-04-03 12:37:36 +0200

    fix: changed controller bindings form xbox 360 to xbox one

--- src/gamepad/controller_mapping.py
+++ src/gamepad/controller_mapping.py
@@ -2,9 +2,9 @@ from enum import StrEnum
 
 class ControllerMapping(StrEnum):
     buttonExit = 'B'
-    L_joystickX = 'LEFT-X'
-    L_joystickY = 'LEFT-Y'
-    R_joystickX = 'RIGHT-X'
-    R_joystickY = 'RIGHT-Y'
+    L_joystickX = 'LAS -X'
+    L_joystickY = 'LAS -Y'
+    R_joystickX = 'RAS -X'
+    R_joystickY = 'RAS -Y'
     throttle = 'LT'
     park = 'RT'
