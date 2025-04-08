1e590f98ae7465ff021b1fb06f7f92d7775999de
autosat-sdc-mihnea <noreply@example.com>
2025-04-09 00:58:01 +0200

    fix: moved some function that were scattered(clean up)

--- src/misc.py
+++ src/misc.py
@@ -1,4 +1,9 @@
-from src.car_variables import CarType
+import logging
+
+from src.car_variables import CarType, HunterFeedbackCanIDs, KartFeedbackCanIDs
+from src.gamepad import Gamepad
+from src.gamepad.controller_mapping import ControllerMapping
+
 
 def calculate_throttle(controller_axis_value: float, car_type: CarType) -> float | None:
     if car_type == CarType.kart:
@@ -13,3 +18,27 @@ def calculate_steering(controller_axis_value: float, car_type: CarType) -> float
         return cubic * 1.25
     elif car_type == CarType.hunter:
         return cubic * 576
+
+def dead_man_switch(gamepad: Gamepad) -> bool:
+    if gamepad.axis(ControllerMapping.park) == 1:
+        park = False
+    else:
+        park = True
+    return park
+
+def controller_break_value(gamepad: Gamepad) -> int:
+    return max(0,-round((gamepad.axis(ControllerMapping.park)**3)*100))
+
+def print_can_messages(message) -> None:
+    print(f"Can Message: {message}\t")
+
+def setup_listeners(can_controller, car_type):
+    """Register CAN message listeners."""
+    if car_type == CarType.hunter:
+        for feedback_id in HunterFeedbackCanIDs:
+            can_controller.add_listener(feedback_id, print_can_messages)
+            logging.debug(f"Listener added for Hunter message ID: {feedback_id}")
+    elif car_type == CarType.kart:
+        for feedback_id in KartFeedbackCanIDs:
+            can_controller.add_listener(feedback_id, print_can_messages)
+            logging.debug(f"Listener added for Kart message ID: {feedback_id}")
