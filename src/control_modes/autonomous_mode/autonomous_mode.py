7bae6458d46f6c438d51c1d56ba355d123b9d1f0
HardstylesDev <getrekttom@gmail.com>
2025-04-16 16:16:11 +0200

    fix: Minor formatting

--- src/control_modes/autonomous_mode/autonomous_mode.py
+++ src/control_modes/autonomous_mode/autonomous_mode.py
@@ -108,10 +108,6 @@ async def start(self):
             steering_angle += avoidance_steering
             speed *= speed_scale
 
-            #if at crosswalk, ignore steering angle altogether
-
-
-
             # Draw everything
             self.renderer.draw(stitched_frame)
 
