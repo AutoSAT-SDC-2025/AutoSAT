9b9940666a0952144ec6978fc17c095cf242784b
mihneabastea <37101806+0Unkn0wn@users.noreply.github.com>
2025-05-14 11:27:16 +0200

    feat: added the ability to see the stitched images

--- src/main.py
+++ src/main.py
@@ -6,13 +6,13 @@ from src.control_modes.manual_mode.manual_mode import ManualMode
 # from src.car_variables import CarType
 # from src.control_modes.manual_mode.manual_mode import ManualMode
 # from src.control_modes.autonomous_mode.autonomous_mode import AutonomousMode
-from src.util.Calibrate import calibrate_connected_cameras
+from src.util.Calibrate import calibrate_connected_cameras, transform_camera_image
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    mode = input("Which mode do you want to launch. 1 for manual, 2 for autonomous, 3 to calibrate: ")
+    mode = input("Which mode do you want to launch. 1 for manual, 2 for autonomous, 3 to calibrate, 4 to pray: ")
     try:
-        if mode not in ["3"]: # maybe more later.
+        if mode not in ["3"] and mode not in ["4"]: # maybe more later.
             car = int(input("Which car are we using: 0 for kart, 1 for hunter: "))
             if car not in [0, 1]:
                 raise ValueError("Invalid car type. Please enter 0 for kart or 1 for hunter.")
@@ -31,6 +31,9 @@ def main():
     elif mode == "3":
         calibrate_connected_cameras(save_path="assets/calibration")
     # go.main()
+
+    elif mode == "4":
+        transform_camera_image()
 
 if __name__ == "__main__":
     main()
