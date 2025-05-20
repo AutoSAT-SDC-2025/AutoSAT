8a5a3a2d284a6e3a418e74bea1034af9c31961a6
mihneabastea <37101806+0Unkn0wn@users.noreply.github.com>
2025-05-21 01:08:52 +0200

    chore: cleanup same as other branches

--- src/control_modes/autonomous_mode/old_twente_code/constants.py
+++
@@ -1,9 +0,0 @@
-CAN_MSG_SENDING_SPEED = 0.040
-height = 480    #480
-width = 640     #848
-camera_buffer_size = 1
-scale = 1
-OBJECT_FPS_CAP = 20
-OBJECT_DOWNSAMPLE_FACTOR = 1
-LOG_ROOT = "logs"
-FALLBACK_VIDEO_PATH = "src/control_modes/autonomous_mode/old_twente_code/recording 13-06-2024 11-54-31.mp4"
