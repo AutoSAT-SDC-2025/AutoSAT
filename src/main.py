a9abc2837fbf19d1e784f322b580fb0a3527dc30
HardstylesDev <getrekttom@gmail.com>
2025-05-20 11:35:08 +0200

    feat: Save used frames

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import logging
 from src.car_variables import CarType
 from src.control_modes.autonomous_mode.autonomous_mode import AutonomousMode
 from src.control_modes.manual_mode.manual_mode import ManualMode
-from src.control_modes.recording_mode.recording_mode import RecordingMode
+from src.control_modes.recording_mode.recording_mode import RecordMode
 # from src.car_variables import CarType
 # from src.control_modes.manual_mode.manual_mode import ManualMode
 # from src.control_modes.autonomous_mode.autonomous_mode import AutonomousMode
@@ -11,7 +11,7 @@ from src.util.Calibrate import calibrate_connected_cameras, transform_camera_ima
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    mode = input("Which mode do you want to launch. 1 for manual, 2 for autonomous, 3 to calibrate, 4 to pray: ")
+    mode = input("Which mode do you want to launch. 1 for manual, 2 for autonomous, 3 for recording, 4 to calibrate, 5 to pray: ")
     try:
         if mode not in ["4"] and mode not in ["5"]: # maybe more later.
             car = int(input("Which car are we using: 0 for kart, 1 for hunter: "))
@@ -29,7 +29,7 @@ def main():
         auto = AutonomousMode(CarType(car))
         auto.start()
     elif mode == "3":
-        record = RecordingMode(CarType(car))
+        record = RecordMode(CarType(car))
         record.start()
 
     elif mode == "4":
