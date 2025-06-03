0fb8185f1f4aee87e74b95c8c36bf646f747ae93
mihneabastea <37101806+0Unkn0wn@users.noreply.github.com>
2025-06-03 15:26:35 +0200

    fix: removed unused lines

--- src/main.py
+++ src/main.py
@@ -1,31 +1,3 @@
-# import logging
-# from src.car_variables import CarType
-# from src.control_modes.manual_mode.manual_mode import ManualMode
-# from src.control_modes.autonomous_mode.autonomous_mode import AutonomousMode
-#
-#
-# def main():
-#     logging.basicConfig(level=logging.INFO)
-#     mode = input("Which mode do you want to launch. 1 for manual, 2 for autonomous: ")
-#     try:
-#         car = int(input("Which car are we using: 0 for kart, 1 for hunter: "))
-#         if car not in [0, 1]:
-#             raise ValueError("Invalid car type. Please enter 0 for kart or 1 for hunter.")
-#     except ValueError as e:
-#         print(e)
-#         return
-#
-#     if mode == "1":
-#         manual = ManualMode(CarType(car))
-#         manual.start()
-#     elif mode == "2":
-#         auto = AutonomousMode(CarType(car))
-#         auto.start()
-#
-# if __name__ == "__main__":
-#     main()
-
-
 import logging
 import uvicorn
 
