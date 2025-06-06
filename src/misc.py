11447e408717cfb741971a4200c016e2834956a9
mihneabastea <37101806+0Unkn0wn@users.noreply.github.com>
2025-06-06 10:48:25 +0200

    fix: reverted calculate_throttle; reversed calculate steering for the kart(idk why its swapped now); reversed the dead_man_switch should be renamed now because it is no longer one

--- src/misc.py
+++ src/misc.py
@@ -6,26 +6,27 @@ from src.can_interface.can_decoder import print_can_messages, broadcast_can_mess
 
 def calculate_throttle(controller_axis_value: float, car_type: CarType) -> float | None:
     if car_type == CarType.kart:
-        return (-controller_axis_value ** 3) * 100
+        x = max(0.0, -controller_axis_value)
+        return (x ** 3) * 100
     elif car_type == CarType.hunter:
         return -(controller_axis_value ** 3) * 1500
 
 def calculate_steering(controller_axis_value: float, car_type: CarType) -> float | None:
     cubic = -(controller_axis_value ** 3)
     if car_type == CarType.kart:
-        return cubic * 1.25
+        return -cubic * 1.25
     elif car_type == CarType.hunter:
         return cubic * 576
 
 def dead_man_switch(gamepad: Gamepad) -> bool:
     if gamepad.axis(ControllerMapping.park) == 1:
-        park = False
-    else:
         park = True
+    else:
+        park = False
     return park
 
 def controller_break_value(gamepad: Gamepad) -> int:
-    return max(0,-round((gamepad.axis(ControllerMapping.park)**3)*100))
+    return max(0,round((gamepad.axis(ControllerMapping.park)**3)*100))
 
 def setup_listeners(can_controller, car_type):
     """Register CAN message listeners."""
