7d09fab0fa41e6ae4485159dc840b3ccd422a2ea
Tom-B31 <tombijenn@gmail.com>
2025-06-11 18:02:52 +0200

    revert: disable debug

--- src/control_modes/autonomous_mode/autonomous_mode.py
+++ src/control_modes/autonomous_mode/autonomous_mode.py
@@ -149,8 +149,9 @@ class AutonomousMode(IControlMode):
                         self.can_controller.set_break(0)
                         self.can_controller.set_kart_gearbox(KartGearBox.forward)
                         self.can_controller.set_throttle(30)
-                        normalized_steering = normalize_steering(steering_angle, 1.25)
-                        self.can_controller.set_steering(normalized_steering)
+                        rad_steer = steering_angle * np.pi / 180
+                        rad_steer = max((min(rad_steer, -1.25)), 1.25)
+                        self.can_controller.set_steering(rad_steer)
         except Exception as e:
             logging.error(f"Error in autonomous mode: {e}")
         finally:
