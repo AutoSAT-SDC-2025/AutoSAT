e3434c4b3e87c02701976fec89f0c4dda342c07a
Tom-B31 <tombijenn@gmail.com>
2025-06-16 10:48:04 +0200

    feat: Implement comments on object detection's utils.py

---
+++ src/control_modes/autonomous_mode/navigation/Navigation.py
@@ -0,0 +1,3 @@
+# Checkpoint based navigation module for autonomous mode.
+# The checkpoint system is implemented, but we haven't yet implemented the logic to use it because
+# localization wasn't properly implemented.
